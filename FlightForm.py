1e96a08fcd5574c1d09aa10f3593cf8d6c1b8d89
Hildermes José Medeiros Filho <hildermes@gmail.com>
2021-02-15 10:47:06 -0300

    Moding FlightForm.py to use UIC aproach
    Loading with UIC

---
+++ FlightForm.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+#  TerrainForm.py
+#  
+#  Copyright 2021 Badolato <badolato@Medusa>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+#!/usr/bin/env python3
+
+import resources      
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import sys
+
+from uis import EfotoDoubleSpinBox
+
+#print(EfotoDoubleSpinBoxModule.__file__)
+
+
+
+class FlightForm(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(FlightForm, self).__init__(parent)
+        sys.modules['EfotoDoubleSpinBox'] = EfotoDoubleSpinBox
+        uic.loadUi('uis/FlightLayout.ui', self)
+
+        self.show()
+
+    def fillValues(self):
+        pass
+
+    def getValues(self):
+        pass
+
+    def setReadOnly(self):
+        pass
+
+    def cleanForm(self):
+        pass
+
+
+
+def main():
+    App = QtWidgets.QApplication(sys.argv)
+    window = FlightForm()
+    sys.exit(App.exec_())
+
+if __name__ == "__main__":
+    main()
